b2c7cdc3991d00eb858648bf3ec0c61fe0a8a376
HSIEH YU JEN <39560035+a5372935@users.noreply.github.com>
2020-10-10 04:31:26 +0800

    Update Image_processes.py

--- Good_NG_tfrecords_process/Image_processes.py
+++ Good_NG_tfrecords_process/Image_processes.py
@@ -7,24 +7,24 @@ import numpy as np
  
 
 cwd = 'C:\\Users\\ASUS\\Desktop\\aoiaidata\\sample\\valid'
-classes = {'NG','Good'}  #人为设定2类
+classes = {'NG','Good'}  #人為設定2類
 
-writer= tf.python_io.TFRecordWriter("GOOD_and_NG_valid_v2.tfrecords") #要生成的文件
+writer= tf.python_io.TFRecordWriter("GOOD_and_NG_valid_v2.tfrecords") #要生成的檔
 
 for index, name in enumerate(classes):
     class_path = cwd + '\\' + name + '\\'
     for img_name in os.listdir(class_path): 
-        img_path = class_path + img_name #每一个图片的地址
+        img_path = class_path + img_name #每一個圖片的位址
         print(img_path)
         img = Image.open(img_path)
         img = img.resize((128,128))
         print(np.shape(img))
         print(index, name)
-        img_raw = img.tobytes() #将图片转化为二进制格式
+        img_raw = img.tobytes() #將圖片轉化為二進位格式
         example = tf.train.Example(features=tf.train.Features(
             feature={"label": tf.train.Feature(int64_list = tf.train.Int64List (value=[index])),
                     'img_raw': tf.train.Feature(bytes_list = tf.train.BytesList (value=[img_raw]))
-        })) #example对象对label和image数据进行封装
-        writer.write(example.SerializeToString())  #序列化为字符串
+        })) #example物件對label和image資料進行封裝
+        writer.write(example.SerializeToString())  #序列化為字串
  
 writer.close()
